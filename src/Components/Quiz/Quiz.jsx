489e46c9b6e3152dd1a42207a27e1a6925fc9d37
karthikiki <karthickcks003@gmail.com>
2025-03-18 22:35:25 +0530

    first

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from 'react'
+import './Quiz.css'
+import { data } from '../../assets/data';
+
+export const Quiz = () => {
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock, setLock] = useState(false);
+
+    let option1 = useRef(null)
+    let option2 = useRef(null)
+    let option3 = useRef(null)
+    let option4 = useRef(null)
+
+    let option_array = [option1,option2,option3,option4];
+
+
+    
+    const checkAnswer = (e, answer) => {
+        if (lock === false) {
+            if (question.Answer === answer) {
+                e.target.classList.add("correct");
+                setLock(true);
+            } else {
+                e.target.classList.add("wrong")
+                setLock(true);
+                option_array[question.Answer - 1].current.classList.add("correct");
+            }
+        }
+    }
+    return (
+        <div className='container'>
+            <h1>Quiz App</h1>
+            <hr />
+            <h2>{index + 1}. {question.question}</h2>
+            <ul>
+                <li ref={option1}  onClick={(e) => { checkAnswer(e, 1) }}>{question.option1}</li>
+                <li ref={option2} onClick={(e) => { checkAnswer(e, 2) }}>{question.option2}</li>
+                <li ref={option3} onClick={(e) => { checkAnswer(e, 3) }}>{question.option3}</li>
+                <li ref={option4} onClick={(e) => { checkAnswer(e, 4) }}>{question.option4}</li>
+            </ul>
+            <button>Next</button>
+            <div className="index">
+                1 of 5 questions
+            </div>
+        </div>
+    )
+}
---
+++ src/assets/data.js
@@ -0,0 +1,39 @@
+export const data = [
+    {
+        question: "What is the capital of France?",
+        option1: "London",
+        option2: "Paris",
+        option3: "Rome",
+        option4: "Berlin",
+        Answer: 2,
+    }, {
+        question: "Who wrote the play Romeo and Juliet?",
+        option1: "William Shakespeare",
+        option2: "Charles Dickens",
+        option3: "Mark Twain",
+        option4: "Jane Austen",
+        Answer: 1
+    }, {
+        question: "What is the largest planet in our solar system?",
+        option1: "Earth",
+        option2: "Jupiter",
+        option3: "Saturn",
+        option4: "Neptune",
+        Answer: 2
+    }, {
+        question: "Which element has the chemical symbol 'O'?",
+        option1: "Oxygen",
+        option2: "Osmium",
+        option3: "Ozone",
+        option4: "Oganesson",
+        Answer: 1
+    }, {
+        Question: "Who was the first president of the United States?",
+        option1: "Thomas Jefferson",
+        option2: "George Washington",
+        option3: "Abraham Lincoln",
+        option4: "John Adams",
+        Answer: 2
+    }
+
+]
